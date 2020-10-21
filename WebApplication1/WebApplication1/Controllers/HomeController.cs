04a29b3fa84061965d8d7a0b9092ad00afce44e7
Wup-Wup <55537968+Wup-Wup@users.noreply.github.com>
2020-10-21 16:03:32 +0200

    Add files via upload

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+    }
+}
