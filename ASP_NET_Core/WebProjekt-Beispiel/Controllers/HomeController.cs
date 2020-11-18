c0952d2f367bc6d8202e7e0ff379483e0d5896ff
Wup wup <bertonialex63@gmail.com>
2020-11-18 14:06:49 +0100

    First

---
+++ ASP_NET_Core/WebProjekt-Beispiel/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebProjekt_Beispiel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
